acf00dbfd64df50aa09ecc4379af6f4626f21a80
Omar Andres Zapata Mesa <andresete.chaos@gmail.com>
2013-06-24 20:05:37 -0500

    added example for minimization and added documentation in TRFuncition.cxx

--- r/src/TRFunction.cxx
+++ r/src/TRFunction.cxx
@@ -12,14 +12,120 @@
 #include<TRFunction.h>
 //______________________________________________________________________________
 /* Begin_Html
-<center><h2>InternalFunction class</h2></center>
+<center><h2>TRFunction class</h2></center>
 
 <p>
 The TRFunction's class lets you to pass ROOT functions to R enviroment<br>
 </p>
+<p>
+The next example was based  in <br>
+<a href="http://root.cern.ch/root/html/tutorials/fit/NumericalMinimization.C.html">
+http://root.cern.ch/root/html/tutorials/fit/NumericalMinimization.C.html
+</a><br>
+<a href="http://stat.ethz.ch/R-manual/R-devel/library/stats/html/optim.html">
+http://stat.ethz.ch/R-manual/R-devel/library/stats/html/optim.html</a><br>
+
+</p>
+End_Html
+Begin_Latex
+
+Let f(x,y)=(x-1)^{2} + 100(y-x^{2})^{2}, which is called the Rosenbrock
+function.
+
+This function is 0 when y = x^{2} , and x = 1, so (1, 1) is a minimum. 
+Let's get the minimum using optim package from R through ROOT R interface.
+In the code this function was called "Double_t RosenBrock(const TVectorD xx )", because for
+optim the input in your function deﬁnition must be a single vector.
+
+
+The Hessian is formed by
+#frac{#partial f}{#partial x} =  -400x(y - x^{2}) - 2(1 - x)
+
+#frac{#partial f}{#partial y} =  200(y - x^{2});
+
+In the code "TVectorD RosenBrockGrad(const TVectorD xx )", because
+in the same way that RosenBrock's definition the input in your function must be a single vector
+and the return too.(see code below)
+
+
+End_Latex
+Begin_Html
+<p>
+<i style="color:red;">NOTE</i>  The macro requires compilation with ACLiC, to compile it you should to use <br>
+a rootlogon.C macro that have the directives for compilation,<br>
+you can find it in $ROOTSYS/tutorials/r/ <br> 
+</p>
 <hr>
 End_Html
-*/
+
+#include<TRInterface.h>
+
+//in the next function the pointer *double should be change by TVectorD, because the pointer has no
+//sense into R enviroment.
+Double_t RosenBrock(const TVectorD xx )
+{
+  const Double_t x = xx[0];
+  const Double_t y = xx[1];
+  const Double_t tmp1 = y-x*x;
+  const Double_t tmp2 = 1-x;
+  return 100*tmp1*tmp1+tmp2*tmp2;
+}
+
+TVectorD RosenBrockGrad(const TVectorD xx )
+{
+  const Double_t x = xx[0];
+  const Double_t y = xx[1];
+  TVectorD grad(2);
+  grad[0]=-400 * x * (y - x * x) - 2 * (1 - x);
+  grad[1]=200 * (y - x * x);
+  return grad;
+}
+
+
+void NumericalMinimization()
+{
+#if defined(__CINT__) && !defined(__MAKECINT__) 
+  cout << "WARNING: This tutorial can run only using ACliC, you must run it by doing: " << endl;
+  cout << "cd  $ROOTSYS/tutorials/r/" << endl;
+  cout << "\t .x $ROOTSYS/tutorials/r/NumericalMinimization.C+" << endl; 
+  return;
+#endif
+ gR->SetVerbose(kFALSE);//Set this KTRUE to show R messages
+ 
+ //passsing RosenBrock funtion to R
+ gR->assign(ROOT::R::TRFunction(RosenBrock),"RosenBrock");
+ 
+ //passsing RosenBrockGrad funtion to R
+ gR->assign(ROOT::R::TRFunction(RosenBrockGrad),"RosenBrockGrad");
+ 
+ //the option "method" could be "Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN","Brent"
+ //the option "control" let you put some constraints like 
+ //"maxit" The maximum number of iterations
+ //"abstol" The absolute convergence tolerance.
+ //"reltol" Relative convergence tolerance.
+ gR->parse("result <- optim( c(0.01,0.01), RosenBrock,method='BFGS',control = list(maxit = 1000000) )");
+ 
+ //Getting result from R
+ TVectorD  min=gR->parseEval("result$par").toVector<Double_t>();
+ 
+ //printing results
+ std::cout<<"-----------------------------------------"<<std::endl;
+ std::cout<<"Minimum x="<<min[0]<<" y="<<min[1]<<std::endl;
+ std::cout<<"Value at minimum ="<<RosenBrock(min)<<std::endl;
+ 
+ //using the hessian
+ gR->parse("optimHess(result$par, RosenBrock, RosenBrockGrad)");
+ gR->parse("hresult <- optim(c(-1.2,1), RosenBrock, NULL, method = 'BFGS', hessian = TRUE)");
+ //getting the min calculated with the hessian
+ TVectorD  hmin=gR->parseEval("hresult$par").toVector<Double_t>();
+
+ //printing results
+ std::cout<<"-----------------------------------------"<<std::endl;
+ std::cout<<"Minization with the Hessian"<<endl;
+ std::cout<<"Minimum x="<<hmin[0]<<" y="<<hmin[1]<<std::endl;
+ std::cout<<"Value at minimum ="<<RosenBrock(hmin)<<std::endl;
+ 
+}*/
 
 
 using namespace ROOT::R;
--- tutorials/r/NumericalMinimization.C
+++ tutorials/r/NumericalMinimization.C
@@ -1,9 +1,11 @@
-//Example taken from 
+//Example based  in
 //http://root.cern.ch/root/html/tutorials/fit/NumericalMinimization.C.html
+//http://stat.ethz.ch/R-manual/R-devel/library/stats/html/optim.html
+//Author: Omar Zapata
 
 #include<TRInterface.h>
 
-//in the next function *double should be change by TVectorD, beacuse the pointer has no
+//in the next function the pointer *double should be change by TVectorD, because the pointer has no
 //sense into R enviroment.
 Double_t RosenBrock(const TVectorD xx )
 {
@@ -14,6 +16,17 @@ Double_t RosenBrock(const TVectorD xx )
   return 100*tmp1*tmp1+tmp2*tmp2;
 }
 
+TVectorD RosenBrockGrad(const TVectorD xx )
+{
+  const Double_t x = xx[0];
+  const Double_t y = xx[1];
+  TVectorD grad(2);
+  grad[0]=-400 * x * (y - x * x) - 2 * (1 - x);
+  grad[1]=200 * (y - x * x);
+  return grad;
+}
+
+
 void NumericalMinimization()
 {
 #if defined(__CINT__) && !defined(__MAKECINT__) 
@@ -22,11 +35,38 @@ void NumericalMinimization()
   return;
 #endif
  gR->SetVerbose(kFALSE);//Set this KTRUE to show R messages
+ 
+ //passsing RosenBrock funtion to R
  gR->assign(ROOT::R::TRFunction(RosenBrock),"RosenBrock");
- //the option mothod could be "Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN","Brent"
- gR->parse("result <- optim( c(0,0), RosenBrock,method='BFGS' )");
+ 
+ //passsing RosenBrockGrad funtion to R
+ gR->assign(ROOT::R::TRFunction(RosenBrockGrad),"RosenBrockGrad");
+ 
+ //the option "method" could be "Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN","Brent"
+ //the option "control" let you put some constraints like 
+ //"maxit" The maximum number of iterations
+ //"abstol" The absolute convergence tolerance.
+ //"reltol" Relative convergence tolerance.
+ gR->parse("result <- optim( c(0.01,0.01), RosenBrock,method='BFGS',control = list(maxit = 1000000) )");
+ 
+ //Getting result from R
  TVectorD  min=gR->parseEval("result$par").toVector<Double_t>();
  
+ //printing results
+ std::cout<<"-----------------------------------------"<<std::endl;
  std::cout<<"Minimum x="<<min[0]<<" y="<<min[1]<<std::endl;
  std::cout<<"Value at minimum ="<<RosenBrock(min)<<std::endl;
+ 
+ //using the hessian
+ gR->parse("optimHess(result$par, RosenBrock, RosenBrockGrad)");
+ gR->parse("hresult <- optim(c(-1.2,1), RosenBrock, NULL, method = 'BFGS', hessian = TRUE)");
+ //getting the min calculated with the hessian
+ TVectorD  hmin=gR->parseEval("hresult$par").toVector<Double_t>();
+
+ //printing results
+ std::cout<<"-----------------------------------------"<<std::endl;
+ std::cout<<"Minization with the Hessian"<<endl;
+ std::cout<<"Minimum x="<<hmin[0]<<" y="<<hmin[1]<<std::endl;
+ std::cout<<"Value at minimum ="<<RosenBrock(hmin)<<std::endl;
+ 
 }
