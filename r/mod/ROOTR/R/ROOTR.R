180e721a356aa6b9e3df833376781cc50c1b9c97
Omar Andres Zapata Mesa <andresete.chaos@gmail.com>
2014-06-05 22:29:31 -0500

    ROOTR: added de R module to call ROOT in R envoronment using library(ROOTR), with this module you can call ROOT's classes in R's interpreter.

---
+++ r/mod/ROOTR/R/ROOTR.R
@@ -0,0 +1,13 @@
+require( Rcpp )
+ROOTSYS           <- Sys.getenv("ROOTSYS")
+ROOTRLIB          <- dyn.load(paste(ROOTSYS,'lib/libRInterface.so',sep='/'))
+ROOTR_TRF1        <- Module("ROOTR_TRF1", ROOTRLIB)
+ROOTR_TRGraph     <- Module("ROOTR_TRGraph", ROOTRLIB)
+ROOTR_TRCanvas    <- Module("ROOTR_TRCanvas", ROOTRLIB)
+ROOTR_TRRint      <- Module("ROOTR_TRRint", ROOTRLIB)
+
+ROOTR <- c()
+ROOTR$TRF1      <- function(name,formula){ new(ROOTR_TRF1$TRF1, name, formula) } #X11 eventloop not working and Draw dont work
+ROOTR$TRGraph   <- function(n,x,y){ new(ROOTR_TRGraph$TRGraph, n,x,y) } #X11 eventloop not working and Draw dont work
+ROOTR$TRCanvas  <- function(name,tittle=''){ new(ROOTR_TRCanvas$TRCanvas, name,tittle) } #X11 eventloop not working and Draw dont work
+ROOTR$TRRint    <- function(name,args=c('')){ new(ROOTR_TRRint$TRRint, name,args) } 
