79120ce4ec4d2268b79919de2a309224014b6fe9
Omar Andres Zapata Mesa <andresete.chaos@gmail.com>
2014-08-10 00:02:06 -0500

    ROOTR: improved Functor example to explain how to export a class

--- tutorials/r/Functor.C
+++ tutorials/r/Functor.C
@@ -9,15 +9,25 @@ typedef Double_t (*Function)(Double_t);
 class MyFunctor{
 public:
   MyFunctor(){
-    //here is the function that I want.
+    status=false;
     f=TMath::BesselY1;
   }
+  void setFunction(Function fun)
+  {
+    f=fun;
+    status=true;
+  }
+  Bool_t getStatus(){return status;}
   Double_t doEval(Double_t x) {
     return f(x);
   }
 private:
    Function f;
+   Bool_t status;
 };
+//this macro expose the class into R's enviornment
+// and let you pass objects directly.
+ROOTR_EXPOSED_CLASS(MyFunctor)
 
 //Macro to create a module
 ROOTR_MODULE(MyFunctorModule) {
@@ -26,13 +36,17 @@ ROOTR_MODULE(MyFunctorModule) {
     .constructor()
     //adding the method doEval to eval the internal function
     .method( "doEval", &MyFunctor::doEval )
+    .method( "getStatus", &MyFunctor::getStatus)
     ;
 }
  
 void Functor()
 {
    ROOT::R::TRInterface &r=ROOT::R::TRInterface::Instance();
-   
+   //////////////////////////////////////////////////////////
+   //Creating functor with deafult function TMath::BesselY1//
+   // and status false from R's environment                //             
+   //////////////////////////////////////////////////////////
    //Loading module into R's enviornment
    r["MyFunctorModule"]<<LOAD_ROOTR_MODULE(MyFunctorModule);
    
@@ -40,8 +54,24 @@ void Functor()
    r<<"MyFunctor <- MyFunctorModule$MyFunctor";
    //creating a MyFunctor's object
    r<<"u <- new(MyFunctor)";
+
+   //printing status
+   r<<"print(u$getStatus())";
    
    //printing values from Functor and Function
    r<<"print(sprintf('value in R = %f',u$doEval( 1 )))";
    std::cout<<"value in ROOT = "<<TMath::BesselY1(1)<<std::endl;
+   
+   ////////////////////////////////////////////////////////////
+   //creating a MyFunctor's object and passing object to R's //
+   //enviornment, the status should be true because is not   //
+   //using the default function                              //
+   ////////////////////////////////////////////////////////////
+   MyFunctor functor;
+   functor.setFunction(TMath::Erf);
+   r["functor"]<<functor;
+   //printing the status that should be true
+   r<<"print(functor$getStatus())";
+   r<<"print(sprintf('value in R = %f',functor$doEval( 1 )))";
+   std::cout<<"value in ROOT = "<<TMath::Erf(1)<<std::endl;
 }
